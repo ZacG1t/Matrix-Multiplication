508bf99507e15de74f29292016699f5acc1540c8
ZacG1t <teozhelun@gmail.com>
2023-07-28 15:44:11 +0800

    Added not working algorithm

--- Matrix_Multiplication.cpp
+++ Matrix_Multiplication.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+#define N 50
 using namespace std; 
 
 void main() {
-	int i, j, k, l, x, y;
-	double A[50][50], B[50][50], Z[50][50];
+	int i, j, k, l, x, y, a = 1;
+	double A[N + 1][N + 1], B[N + 1][N + 1], Z[N + 1][N + 1];
 
 	/* Ask user for matrix dimension */
 	cout << "What is the dimension of matrix A? " << endl;
@@ -29,21 +30,24 @@ void main() {
 			}
 		}
 
+		/*
 		Z[1][1] = A[1][1] * B[1][1] + A[1][2] * B[2][1] + A[1][3] * B[3][1]; Z[1][2] = A[1][1] * B[1][2] + A[1][2] * B[2][2] + A[1][3] * B[3][2]; Z[1][3] = A[1][1] * B[1][3] + A[1][2] * B[2][3] + A[1][3] * B[3][3];
 		Z[2][1] = A[2][1] * B[1][1] + A[2][2] * B[2][1] + A[2][3] * B[3][1]; Z[2][2] = A[2][1] * B[1][2] + A[2][2] * B[2][2] + A[2][3] * B[3][2]; Z[2][3] = A[2][1] * B[1][3] + A[2][2] * B[2][3] + A[2][3] * B[3][3];
-		Z[3][1] = A[3][1] * B[1][1] + A[3][2] * B[2][1] + A[3][3] * B[3][1];
+		Z[3][1] = A[3][1] * B[1][1] + A[3][2] * B[2][1] + A[3][3] * B[3][1]; Z[3][2] = A[3][1] * B[1][2] + A[3][2] * B[2][2] + A[3][3] * B[3][2]; Z[3][3] = A[3][1] * B[1][3] + A[3][2] * B[2][3] + A[3][3] * B[3][3];
+		*/
+		cout << "\nCounting..." << endl;
 		for (x = 1; x <= i; x++) {
-			Z[x][x] = A[x][x] * B[x][x];
 			for (y = 1; y <= l; y++) {
-				if (x == y) {
-
+				while (a <= j) {
+					Z[x][y] += A[x][a] * B[a][y];
+					a++;
 				}
-				else if (x != y) {
-
-				}
+				cout << Z[x][y] << "\t";
 			}
+			cout << endl;
 		}
 	}
-	else if (j != k)
+	else if (j != k) {
 		cout << "Dimension of matrix A and matrix B are incompatible. " << endl;
+	}
 }
